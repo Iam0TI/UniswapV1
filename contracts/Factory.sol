ba1cfd2198f95fa994ada249b261a672a1262e4d
JustFavour <odeyemifavour1@gmail.com>
2024-09-05 22:49:53 +0100

    feat: created Factory

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,5 +1,24 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.20;
+pragma solidity 0.8.27;
 
-contract Factory {}
+import {Exchange} from "./Exchange.sol";
+
+contract Factory {
+    error ExchangeExist();
+
+    mapping(address => address) public tokenToExachange;
+
+    function createExchange(address _tokenAddress) external returns (address) {
+        if (tokenToExachange[_tokenAddress] != address(0)) revert ExchangeExist();
+
+        Exchange exchange = new Exchange(_tokenAddress);
+
+        tokenToExachange[_tokenAddress] = address(exchange);
+        return address(exchange);
+    }
+
+    function getExchangeByToken(address _tokenaddress) external view returns (address) {
+        return tokenToExachange[_tokenaddress];
+    }
+}
